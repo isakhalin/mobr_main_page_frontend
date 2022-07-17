7077864ae3be130e3cf8a8c1d0148ecadf8f8eb1
Feikkont <feikkont@gmail.com>
2022-07-17 21:28:31 +1100

    timestamp

--- src/api/ticket.js
+++ src/api/ticket.js
@@ -1,14 +1,21 @@
 import { database } from './firebase'
-import { set, get, child, ref } from 'firebase/database';
+import { set, get, child, ref, serverTimestamp } from 'firebase/database';
 
 export const sendTicketToFirebaseApi = (ticket, uid) => {
-    return set(child(ref(database), `tickets/${uid}/${ticket.ticketDate}`), ticket)
+    const newTicket = {...ticket, newDate: serverTimestamp()} // serverTimestamp дает временную метку сервера
+    console.log('newticket', newTicket) // на данный момент временной метки нет, она появится после добавления в FB
+    return set(child(ref(database), `tickets/${uid}/${newTicket.ticketDate}`), newTicket)
+
+    // return set(child(ref(database), `tickets/${uid}/${ticket.ticketDate}`), ticket)
 };
 
 export const getTicketsFromFirebaseApi = (uid, isAdmin) => {
     if (!isAdmin){
         return get(child(ref(database), `tickets/${uid}`));
     } else {
+        console.log('timestamp:', serverTimestamp())
+
         return get(child(ref(database), "tickets"));
+        // debugger
     }
 };
