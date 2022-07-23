6bee3b599004aff9521961c5dfab6a8ca8e2c4de
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-07-23 17:22:28 +1100

    working on add user profiles from applications

--- src/components/add-users/add-user.js
+++ src/components/add-users/add-user.js
@@ -1,4 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+
+/** Firebase */
+import {createUserWithEmailAndPassword, signOut} from 'firebase/auth';
+import {auth} from "../../api";
 
 /** MUI Material Comps */
 import {
@@ -10,21 +14,27 @@ import {
     FormControlLabel,
 } from "@mui/material";
 
+// const sendNewProfile = (email, pass) => {
+//     return createUserWithEmailAndPassword(auth, email, pass).then(user => user.user.uid);
+// };
+
 export const AddUser = ({application}) => {
     const [newUser, setNewUser] = useState({
-            date: application.date,                 //Данные в апликейшене
-            firstName: application.firstName,
-            lastName: application.lastName,
-            middleName: application.middleName,
-            dept: application.dept,
-            isMinobr: application.isMinobr,
-            org: application.org,
-            prevOrg: application.prevOrg,
-            phoneNumber: application.phoneNumber ?? '',
-            phoneNumberMobile: application.phoneNumberMobile ?? '',
-            position: application.position,
-            room: application.room,
+        //Данные в апликейшене
+        date: application.date,                   // Дата апликейшена. Возможно сохранять в профиль не нужно
+        firstName: application.firstName,
+        lastName: application.lastName,
+        middleName: application.middleName,
+        dept: application.dept,
+        isMinobr: application.isMinobr,
+        org: application.org,
+        prevOrg: application.prevOrg,
+        phoneNumber: application.phoneNumber ?? '',
+        phoneNumberMobile: application.phoneNumberMobile ?? '',
+        position: application.position,
+        room: application.room,
 
+        email: '',
         avatar: "",                                 //+ Данные в профиле
         isAdmin: false,
     });
@@ -33,13 +43,24 @@ export const AddUser = ({application}) => {
         pass: ''
     });
 
-    const sendNewProfile = () => {
-        console.log("NEW USER", newUser)
+    const setMail = () => {
+        let email = 'some@mail.ru';
+        const name = newUser.firstName[0];
+        console.log("Первая бква имени", name)
+        return email
     };
 
-    const createNewUser = () => {
-        sendNewProfile();
+
+    const createNewUser = async () => {
+        const newUserUid = await createUserWithEmailAndPassword(auth, loginPass.login, loginPass.pass).then(user => user.user.uid);
+        // const newUserUid = await sendNewProfile(loginPass.login, loginPass.pass);
+        console.log("newUSER-Uid", newUserUid);
+        await signOut(auth);
     };
+
+    useEffect(() => {
+        setMail()
+    }, [])
 
     return (
         <div>
@@ -86,11 +107,23 @@ export const AddUser = ({application}) => {
                             onChange={(e) => setLoginPass({...loginPass, pass: e.target.value})}
                         />
                     </div>
+                    <div>
+                        <Button
+                            variant="outlined"
+                            onClick={createNewUser}
+                            size="small"
+                            sx={{
+                                fontSize: 12,
+                            }}
+                        >
+                            Сформировать
+                        </Button>
+                    </div>
                 </Box>
-                <Box sx={{'& > :not(style)': {m: 0.2, fontSize: 12}}}>
-                    <FormControlLabel sx={{'& .MuiFormControlLabel-label': {fontSize: 12}}} control={<Checkbox />} label="Сотрудник министерства" />
-                    <Button variant="outlined" onClick={createNewUser}>Сформировать</Button>
-                </Box>
+                {/*<Box sx={{'& > :not(style)': {m: 0.2, fontSize: 12}}}>*/}
+                {/*    /!*<FormControlLabel sx={{'& .MuiFormControlLabel-label': {fontSize: 12}}} control={<Checkbox />} label="Сотрудник министерства" />*!/*/}
+                {/*    <Button variant="outlined" onClick={createNewUser}>Сформировать</Button>*/}
+                {/*</Box>*/}
             </Typography>
         </div>
     );
